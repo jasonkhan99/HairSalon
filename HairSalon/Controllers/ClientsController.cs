51a69ba1da3c5a563bab4484c38a54e54e6e9370
jasonderekkhan@gmail.com <jasonderekkhan@gmail.com>
2020-07-31 11:06:12 -0700

    add client delete route and http post to controller

--- HairSalon/Controllers/ClientsController.cs
+++ HairSalon/Controllers/ClientsController.cs
@@ -42,5 +42,20 @@ namespace HairSalon.Controllers
       return View(thisClient);
     }
 
+    public ActionResult Delete(int id)
+    {
+      var thisClient = _db.Clients.FirstOrDefault(clients => clients.ClientId == id);
+      return View(thisClient);
+    }
+
+    [HttpPost, ActionName("Delete")]
+    public ActionResult DeleteConfirmed(int id)
+    {
+      var thisClient = _db.Clients.FirstOrDefault(clients => clients.ClientId == id);
+      _db.Clients.Remove(thisClient);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
   }
 }
